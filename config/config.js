6c033c43d462f0c080369f23503140e1b60c0910
eelok <maria@resourceful-humans.de>
2022-09-16 08:43:40 +0200

    change config.json to js add user migration

--- config/config.js
+++ config/config.js
@@ -1,23 +1,26 @@
-{
+module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
+    "username": "postgres",
+    "password": "goodPassword",
+    "database": "res_admin",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "port": "5433",
+    "dialect": "postgres"
   },
   "test": {
     "username": "root",
     "password": null,
     "database": "database_test",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "port": "5433",
+    "dialect": "postgres"
   },
   "production": {
     "username": "root",
     "password": null,
     "database": "database_production",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "port": "5433",
+    "dialect": "postgres"
   }
 }
