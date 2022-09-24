faf107527631da29d187829876e0b3420b8b218a
eelok <maria@resourceful-humans.de>
2022-09-24 15:04:53 +0200

    association user education wip

--- models/education.js
+++ models/education.js
@@ -10,7 +10,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Education.belongsToMany(models.User, {
+        through: "User_Education"
+      });
     }
   }
   Education.init({
--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@
 const {
   Model
 } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -10,7 +11,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      User.belongsToMany(models.Education, {
+        through: "User_Education"
+      });
     }
   }
   User.init({
