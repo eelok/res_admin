27dde87e1c5739cdd3f818f1eeeacb5a33ec25b6
eelok <maria@resourceful-humans.de>
2022-09-24 11:06:34 +0200

    import db through context

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const { createServer } = require('@graphql-yoga/node');
 const { v4: uuidv4 } = require('uuid');
 const constants = require('./constants');
-const {User, Education, Hardskill} = require('../models');
 const fs = require('fs');
+const db = require('../models');
 
 const resolvers = {
     Query: {
@@ -15,52 +15,54 @@ const resolvers = {
         }
     },
     Mutation: {
-        async createUser(parent, args, ctx, info){
-            const {firstName, lastName, email} = args;
+        async createUser(parent, args, ctx, info) {
+            const { firstName, lastName, email } = args;
+            const {db} = ctx;
             const id = uuidv4();
-            console.log(id);
-            try{
-                const foundUser = await User.findOne({
-                    where: {email}
+            try {
+                const foundUser = await db.User.findOne({
+                    where: { email }
                 });
-                if(!foundUser){
-                    const newUser = await User.create({id: id, firstName: firstName, lastName: lastName, email});
+                if (!foundUser) {
+                    const newUser = await db.User.create({ id: id, firstName: firstName, lastName: lastName, email });
                     return {
                         code: constants.STATUS_CODE_201,
                         sucess: true,
                         message: 'User with ${id} was created',
                     };
-                } 
-                return {
-                        code: constants.STATUS_CODE_400,
-                        sucess: false,
-                        message: 'User with ${id} is already exists',
                 }
-                
-            } catch(err){
+                return {
+                    code: constants.STATUS_CODE_400,
+                    sucess: false,
+                    message: 'User with ${id} is already exists',
+                }
+
+            } catch (err) {
                 console.log(err);
                 throw err;
             }
         },
-        async createEducation(parent, args, ctx, info){
-            const {start, end, shortName, longName, division, description} = args;
+        async createEducation(parent, args, ctx, info) {
+            const { start, end, shortName, longName, division, description } = args;
+            const {db} = ctx;
             const id = uuidv4();
             try {
-                const newEducaton = await Education.create({id, start, end, shortName, longName, division, description});
+                const newEducaton = await db.Education.create({ id, start, end, shortName, longName, division, description });
                 return newEducaton;
-            } catch(err){
+            } catch (err) {
                 console.log(err);
                 throw err;
             }
 
         },
-        async createHardSkill(parent, args, ctx, info){
-            const {title} = args;
+        async createHardSkill(parent, args, ctx, info) {
+            const { title } = args;
+            const { db } = ctx;
             const id = uuidv4();
             try {
-                const newHardSkill = await Hardskill.create({id, title});
+                const newHardSkill = await db.Hardskill.create({ id, title });
                 return newHardSkill;
-            } catch(err){
+            } catch (err) {
                 console.log(err);
                 throw err;
             }
@@ -69,13 +71,17 @@ const resolvers = {
 }
 
 
-const server = createServer({
-    schema: {
-      typeDefs: fs.readFileSync('./src/schema.graphql', { encoding: "utf8" }),
-      resolvers
-    },
-  })
-  
+const server =
+    createServer({
+        schema: {
+            typeDefs: fs.readFileSync('./src/schema.graphql', { encoding: "utf8" }),
+            resolvers,
+        },
+        context: {
+            db
+        }
+    })
+
 server.start(() => {
     console.log("the server is up")
 });
