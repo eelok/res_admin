dc471e65e560dc24a79de483d017dbb9949c5987
eelok <maria@resourceful-humans.de>
2022-10-08 21:47:39 +0200

    introduce deleteEducation & deleteHardSkill

--- src/util/getUserID.js
+++ src/util/getUserID.js
@@ -2,18 +2,13 @@ const jwt = require('jsonwebtoken');
 const secret = 'mySecret'
 
 const getUserID = (request) => {
-    try{
-        const header = request.req.headers.authorization;
-        if (!header) {
-            throw new GraphQLYogaError("Authenticaton required");
-        }
-        const token = header.split(" ")[1];
-        const decoded = jwt.verify(token, secret);
-        return decoded.id;
-    } catch(err){
-        console.log(err);
-        throw new GraphQLYogaError(err);
+    const header = request.req.headers.authorization;
+    if (!header) {
+        throw new GraphQLYogaError("Authenticaton required");
     }
+    const token = header.split(" ")[1];
+    const decoded = jwt.verify(token, secret);
+    return decoded.id;
 };
 
 module.exports ={
