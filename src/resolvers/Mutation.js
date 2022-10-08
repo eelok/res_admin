dddd6f36db5ef3e00e9c8b008b7617abe60931d7
eelok <maria@resourceful-humans.de>
2022-10-08 18:12:40 +0200

    move addEducationToUser to separate file

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,70 +1,18 @@
 const { where } = require('sequelize');
 const { v4: uuidv4 } = require('uuid');
 const constants = require('../constants');
-const bcrypt = require('bcryptjs');
-const salt = bcrypt.genSaltSync(10);
-const jwt = require('jsonwebtoken');
+
 const { GraphQLYogaError } = require('@graphql-yoga/node');
 const { getUserID } = require('../util/getUserID');
 const {createUser} = require('../resolvers/user/createUser');
 const {loginUser} = require('../resolvers/user/loginUser');
-const secret = 'mySecret'
+const {addEducationToUser} = require('./education/addEducationToUser');
+
 const Mutation = {
     createUser,
     loginUser,
-    // async createUser(parent, args, ctx, info) {
-    //     const { firstName, lastName, email, password } = args;
-    //     const { db } = ctx;
+    addEducationToUser,
 
-    //     if (password.length < 8) {
-    //         throw new GraphQLYogaError('Password must be 8 characters or longer');
-    //     }
-    //     const hashedPassword = await bcrypt.hash(password, salt);
-    //     const id = uuidv4();
-    //     try {
-    //         const foundUser = await db.User.findOne({
-    //             where: { email }
-    //         });
-    //         if (foundUser) {
-    //             throw new GraphQLYogaError('User with the such email is already exists');
-    //         }
-    //         const newUser = await db.User.create({
-    //             id,
-    //             firstName,
-    //             lastName,
-    //             email,
-    //             password: hashedPassword
-    //         });
-    //         return {
-    //             user: newUser,
-    //             token: jwt.sign({ id: newUser.id }, secret)
-    //         }
-    //     } catch (err) {
-    //         console.log(err);
-    //         throw err;
-    //     }
-    // },
-
-    // async loginUser(parent, args, ctx, info) {
-    //     const { email, password } = args;
-    //     const { db } = ctx;
-    //     const foundUser = await db.User.findOne({
-    //         where: {
-    //             email
-    //         }
-    //     });
-    //     if (!foundUser) {
-    //         throw new GraphQLYogaError(`Unable to login`);
-    //     }
-    //     const isMatch = await bcrypt.compare(password, foundUser.password);
-    //     if (!isMatch) {
-    //         throw new GraphQLYogaError('Unable to login');
-    //     }
-    //     return {
-    //         user: foundUser,
-    //         token: jwt.sign({ id: foundUser.id }, secret)
-    //     }
-    // },
     async addHardSkillToUser(parent, args, ctx, info) {
         const { userId, title } = args;
         const { db } = ctx;
@@ -93,35 +41,8 @@ const Mutation = {
         }
     },
 
-    //todo rename addMyEducation
-    async addEducationToUser(parent, args, ctx, info) {
-        const { start, end, shortName, longName, division, description } = args
-        const { db, request } = ctx;
 
-        try {
-            const userID = getUserID(request);
-            const foundUser = await db.User.findByPk(userID);
-            
-            if (!foundUser) {
-                return {
-                    code: constants.STATUS_CODE_404,
-                    sucess: false,
-                    message: `user with id: ${decoded.id} does not exist`
-                };
-            }
-            const id = uuidv4();
-            const education = await db.Education.create({ id, start, end, shortName, longName, division, description });
-            const newUserEducation = await foundUser.addEducation(education, { through: db.User_Education });
-            return {
-                    code: constants.STATUS_CODE_201,
-                    sucess: true,
-                    message: `education wiht id ${education.id} was added to ${foundUser.id}`
-            };
-        } catch (err) {
-            console.log(err);
-            throw new GraphQLYogaError(err);
-        }
-    }
+   
 }
 
 module.exports = {
