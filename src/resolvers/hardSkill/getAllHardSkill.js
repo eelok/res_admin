b8651e04df569fe36ee8e565824ede39e740697e
eelok <maria@resourceful-humans.de>
2022-10-15 16:08:36 +0200

    wip

--- src/resolvers/hardSkill/getAllHardSkill.js
+++ src/resolvers/hardSkill/getAllHardSkill.js
@@ -4,11 +4,11 @@ const getAllHardSkill = async (parent, args, ctx, info) => {
      console.log("Hello");
      const {db, request} = ctx;
      try{
-        const userID = getUserID(request);
-        const foundUser = await db.User.findByPk(userID);
+        // const userID = getUserID(request);
+        // const foundUser = await db.User.findByPk(id);
         const listOfHardSkills = await db.Hardskill.findAll({
             where: {
-                userId: foundUser.id
+                userId: "845eb39c-7bd1-4a17-8756-cad2ee870e8f"
             }
         })
         return listOfHardSkills
